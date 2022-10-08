51f1a0d7fdbb5dca08175981ba771e71c6274e41
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-10-09 01:29:30 +0900

    Add files via upload

---
+++ Exam15.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Grade{
+    int math;
+    int science;
+    int english;
+    public Grade(int math , int science, int english){
+        this.math = math;
+        this.science = science;
+        this.english = english;
+
+    }
+
+
+    public int average() {
+        return((math+science+english)/3);
+    }
+}
+public class Exam15 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("수학 과학 영어 순으로 3개의 점수 입력");
+        int math = in.nextInt();
+        int science = in.nextInt();
+        int english = in.nextInt();
+        Grade me = new Grade(math,science,english);
+        System.out.println("평균은 " +me.average());
+
+        in.close();
+        }
+}
+
+
+
+
